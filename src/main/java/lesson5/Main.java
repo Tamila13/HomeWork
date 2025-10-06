a3df31e328ce9e097639d9d4832b3f0d274476e6
DESKTOP-UQB94AB\User <nm.tamila@gmail.com>
2025-10-06 14:55:22 +0300

    lesson5

--- src/main/java/lesson5/Main.java
+++ src/main/java/lesson5/Main.java
@@ -1,2 +1,30 @@
-package main.java.lesson5;public class App {
+package main.java.lesson5;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть суму доходу: ");
+        double income = scanner.nextDouble();
+
+        double taxRate;
+        double taxAmount;
+
+        if (income <= 10000){
+            taxRate = 2.5;
+        } else if (income <= 25000) {
+            taxRate = 4.3;
+        } else {
+            taxRate = 6.7;
+        }
+
+        taxAmount = income*taxRate/100;
+
+        System.out.printf("Сума доходу: %.2f грн%n", income);
+        System.out.printf("Ставка податку: %.1f%%%n", taxRate);
+        System.out.printf("Сума податку: %.2f грн%n", taxAmount);
+
+    }
+
 }
